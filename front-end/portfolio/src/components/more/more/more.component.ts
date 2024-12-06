0006c2fa85620f31076a4d8f03c0c156eb5271f7
sivani-tutika <sivanitutika4@gmail.com>
2024-12-06 16:45:19 -0600

    sectioning the project

---
+++ front-end/portfolio/src/components/more/more/more.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-more',
+  imports: [],
+  templateUrl: './more.component.html',
+  styleUrl: './more.component.css'
+})
+export class MoreComponent {
+
+}
