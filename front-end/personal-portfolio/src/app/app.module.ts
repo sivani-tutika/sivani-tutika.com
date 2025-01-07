b6e7535c359400f3d6d5ce561daeb22f5fb57fb6
sivani-tutika <sivanitutika4@gmail.com>
2025-01-07 13:13:41 -0600

    HTML outline done

---
+++ front-end/personal-portfolio/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MoreComponent } from './components/more/more.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent,
+    MoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync('noop')
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front-end/personal-portfolio/src/app/components/work/work.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work',
+  standalone: false,
+  
+  templateUrl: './work.component.html',
+  styleUrl: './work.component.css'
+})
+export class WorkComponent {
+
+}
